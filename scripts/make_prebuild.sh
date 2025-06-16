1a0049d36fc0427ae90383a9ec0f989d0cacef01
WangJike <525441605@qq.com>
2025-06-16 10:11:32 +0800

    [dev] 优化预构建包打包脚本

--- scripts/make_prebuild.sh
+++ scripts/make_prebuild.sh
@@ -6,9 +6,7 @@ echo "autogen..."
 echo "make ramdisk.sh"
 ./scripts/create_ramdisk.sh || { echo "create ramdisk failed."; exit 1; }
 
-echo "make fs tar"
-tar -vcf output/fs.tar --transform 's|^output/||' output/fs  || { echo "tar failed."; exit 1; }
-
 echo "copy to prebuild"
+test -d prebuild/ || mkdir prebuild/
 find prebuild/ -type f -exec rm -rf {} \;
 find output/ -path "output/fs" -prune -o -type f -exec cp -rf {} prebuild \;
